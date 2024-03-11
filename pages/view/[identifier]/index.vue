882fdcd019484b2a8adc7577de2ca0292f9f03af
Sanjay Soundarajan <sanjay.16@live.com>
2024-03-10 18:09:23 -0700

    🔨 chore: update prefix ordering

--- pages/view/[identifier]/index.vue
+++ pages/view/[identifier]/index.vue
@@ -24,82 +24,88 @@ if (error.value) {
 </script>
 
 <template>
-  <main class="mx-auto max-w-screen-xl grow bg-white px-3 pb-10 pt-5">
-    <div class="grid grid-cols-12">
-      <n-space vertical class="col-span-9 mt-5">
-        <n-space align="center">
-          <h1 class="mb-2">{{ data.collection.title }}</h1>
+  <main class="h-screen w-full grow overflow-auto bg-white px-3 pb-10 pt-5">
+    <div class="mx-auto max-w-screen-xl">
+      <div class="grid grid-cols-12">
+        <n-space vertical class="col-span-9 mt-5">
+          <n-space align="center">
+            <h1 class="mb-2">{{ data.collection.title }}</h1>
 
-          <n-tag type="success" :bordered="false">
-            Version {{ data.name }}
-          </n-tag>
+            <n-tag type="success" :bordered="false">
+              Version {{ data.name }}
+            </n-tag>
+          </n-space>
+
+          <p class="line-clamp-5">{{ data.collection.description }}</p>
+
+          <p class="text-base">
+            Published on
+            {{ displayStandardDate(data.published_on) }}
+          </p>
         </n-space>
 
-        <p class="line-clamp-5">{{ data.collection.description }}</p>
+        <NuxtImg
+          :src="data.collection.image"
+          :alt="data.collection.title"
+          class="col-span-3 h-auto max-h-60 w-full"
+        />
+      </div>
 
-        <p class="text-base">
-          Published on
-          {{ displayStandardDate(data.published_on) }}
-        </p>
-      </n-space>
+      <n-divider />
 
-      <NuxtImg
-        :src="data.collection.image"
-        :alt="data.collection.title"
-        class="col-span-3 h-auto max-h-60 w-full"
-      />
-    </div>
+      <n-tabs type="segment" animated>
+        <n-tab-pane name="resources" tab="Resources">
+          <template #tab>
+            <n-space align="center">
+              <Icon name="fluent:text-bullet-list-square-16-filled" size="18" />
 
-    <n-divider />
+              <span class="font-medium"> Resources</span>
+            </n-space>
+          </template>
 
-    <n-tabs type="segment" animated>
-      <n-tab-pane name="resources" tab="Resources">
-        <template #tab>
-          <n-space align="center">
-            <Icon name="fluent:text-bullet-list-square-16-filled" size="18" />
-
-            <span class="font-medium"> Resources</span>
-          </n-space>
-        </template>
-
-        <pre>
+          <pre>
           {{ data.Resources }}
-        </pre>
-      </n-tab-pane>
+        </pre
+          >
+        </n-tab-pane>
 
-      <n-tab-pane name="versions" tab="Versions">
-        <template #tab>
-          <n-space align="center">
-            <Icon name="system-uicons:versions" size="18" />
+        <n-tab-pane name="versions" tab="Versions">
+          <template #tab>
+            <n-space align="center">
+              <Icon name="system-uicons:versions" size="18" />
 
-            <span class="font-medium"> Versions</span>
-          </n-space>
-        </template>
+              <span class="font-medium"> Versions</span>
+            </n-space>
+          </template>
 
-        <pre>
+          <pre>
           {{ data.Versions }}
-        </pre>
-      </n-tab-pane>
+        </pre
+          >
+        </n-tab-pane>
 
-      <n-tab-pane name="relations" tab="Relations">
-        <template #tab>
-          <n-space align="center">
-            <Icon name="tabler:circles-relation" size="18" />
+        <n-tab-pane name="relations" tab="Relations">
+          <template #tab>
+            <n-space align="center">
+              <Icon name="tabler:circles-relation" size="18" />
 
-            <span class="font-medium"> Relations</span>
-          </n-space>
-        </template>
+              <span class="font-medium"> Relations</span>
+            </n-space>
+          </template>
 
-        Internal Relations
-        <pre>
+          Internal Relations
+          <pre>
          {{ data.InternalRelations }}
-        </pre>
+        </pre
+          >
 
-        External Relations
-        <pre>
+          External Relations
+          <pre>
          {{ data.ExternalRelations }}
-        </pre>
-      </n-tab-pane>
-    </n-tabs>
+        </pre
+          >
+        </n-tab-pane>
+      </n-tabs>
+    </div>
   </main>
 </template>
