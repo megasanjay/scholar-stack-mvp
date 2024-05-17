ad3d2edeeb8a4dcdbe4a4abab763613ea1dbd1b9
Sanjay Soundarajan <sanjay.16@live.com>
2024-05-17 03:03:21 -0700

    🐛 fix: prevent double personal workspace creation

--- server/api/workspaces/index.get.ts
+++ server/api/workspaces/index.get.ts
@@ -1,4 +1,3 @@
-import { nanoid } from "nanoid";
 import { serverSupabaseUser } from "#supabase/server";
 
 export default defineEventHandler(async (event) => {
@@ -7,72 +6,41 @@ export default defineEventHandler(async (event) => {
   const user = await serverSupabaseUser(event);
   const userid = user?.id as string;
 
-  const workspaceId = nanoid();
-
-  return await prisma.$transaction(async (tx) => {
-    // Get the user's workspaces
-
-    const workspaces = await tx.workspaceMember.findMany({
-      select: {
-        workspace: {
-          select: {
-            id: true,
-            title: true,
-            created: true,
-            description: true,
-            personal: true,
-            type: true,
-          },
+  // Get the user's workspaces
+  const workspaces = await prisma.workspaceMember.findMany({
+    select: {
+      workspace: {
+        select: {
+          id: true,
+          title: true,
+          created: true,
+          description: true,
+          personal: true,
+          type: true,
         },
       },
-      where: {
-        user_id: user?.id,
-      },
-    });
+    },
+    where: {
+      user_id: userid,
+    },
+  });
 
-    // Check if a personal workspace already exists
-    const personalWorkspaceExists = workspaces.some(
-      (workspace) => workspace.workspace.personal,
+  if (workspaces && workspaces.length > 0) {
+    const foundWorkspaces = workspaces.map((workspace) => workspace.workspace);
+
+    // Move the personal workspace to the top
+    const personalWorkspace = foundWorkspaces.find(
+      (workspace) => workspace.personal,
     );
 
-    if (workspaces && workspaces.length > 0 && personalWorkspaceExists) {
-      const foundWorkspaces = workspaces.map(
-        (workspace) => workspace.workspace,
-      );
-
-      // Move the personal workspace to the top
-      const personalWorkspace = foundWorkspaces.find(
-        (workspace) => workspace.personal,
-      );
-
-      if (personalWorkspace) {
-        const personalWorkspaceIndex =
-          foundWorkspaces.indexOf(personalWorkspace);
-        foundWorkspaces.splice(personalWorkspaceIndex, 1);
-        foundWorkspaces.unshift(personalWorkspace);
-      }
-
-      return foundWorkspaces;
+    if (personalWorkspace) {
+      const personalWorkspaceIndex = foundWorkspaces.indexOf(personalWorkspace);
+      foundWorkspaces.splice(personalWorkspaceIndex, 1);
+      foundWorkspaces.unshift(personalWorkspace);
     }
 
-    // Create a personal workspace
-    const personalWorkspace = await tx.workspace.create({
-      data: {
-        id: workspaceId,
-        title: "My workspace",
-        description: "This is my personal workspace",
-        personal: true,
-        type: "personal",
-        WorkspaceMember: {
-          create: {
-            admin: false,
-            owner: true,
-            user_id: userid,
-          },
-        },
-      },
-    });
+    return foundWorkspaces || [];
+  }
 
-    return [personalWorkspace];
-  });
+  return [];
 });
