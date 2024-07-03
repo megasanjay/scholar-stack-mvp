eae256147cabdb1ed543cf70d0a40ce43af16429
Sanjay Soundarajan <sanjay.16@live.com>
2024-07-03 02:03:51 -0700

    🐛 fix: source resource get

--- server/api/workspaces/[workspaceid]/collections/[collectionid]/resources/source.get.ts
+++ server/api/workspaces/[workspaceid]/collections/[collectionid]/resources/source.get.ts
@@ -19,92 +19,75 @@ export default defineEventHandler(async (event) => {
   }
 
   // get all the versions for the collection
-  const versions = await prisma.version.findMany({
-    where: {
-      collection_id: collectionid,
-    },
-  });
+  // const versions = await prisma.version.findMany({
+  //   where: {
+  //     collection_id: collectionid,
+  //   },
+  // });
 
   const response: ResourcesList = [];
 
   const allResources = [];
   const allResourceIds = [];
 
-  // get the resources for each version
-
-  for (const version of versions) {
-    const resources = await prisma.resource.findMany({
-      where: {
-        Version: {
-          some: {
-            id: version.id,
-          },
+  // get all the resources for the collection
+  const resources = await prisma.resource.findMany({
+    include: {
+      Version: {
+        select: {
+          name: true,
         },
       },
-    });
-
-    for (const resource of resources) {
-      if (resource.original_resource_id) {
-        // remove the original resource from the list in favor of the cloned resource
-        const originalResourceIndex = allResources.findIndex(
-          (r) => r.id === resource.original_resource_id,
-        );
-
-        if (originalResourceIndex !== -1) {
-          allResources.splice(originalResourceIndex, 1);
-        }
-      }
-
-      if (
-        resource.action &&
-        (resource.action === "delete" || resource.action === "oldVersion")
-      ) {
-        continue;
-      }
-
-      allResources.push({
-        ...resource,
-        versionName: version.name,
-      });
-      allResourceIds.push(resource.id);
-    }
-  }
-
-  // Get a unique list of all the resource ids keeping the order of the first occurrence that is not the draft version
-  const uniqueResourceIds = allResourceIds.filter(
-    (value, index, self) => self.indexOf(value) === index,
-  );
-
-  // Remove the resources that are not in the collection
-  const filteredResources = allResources.filter((resource) =>
-    uniqueResourceIds.includes(resource.id),
-  );
-
-  // sort the resources by the version name in descending order
-  filteredResources.sort((a, b) => {
-    if (a.versionName < b.versionName) {
-      return 1;
-    }
-    if (a.versionName > b.versionName) {
-      return -1;
-    }
-
-    return 0;
+    },
+    where: {
+      Version: {
+        some: {
+          collection_id: collectionid,
+        },
+      },
+    },
   });
 
-  // remove earlier versions of the same resource
+  // Remove any duplicate resources and any resources that have the delete or oldVersion action
   const seen = new Set();
-  const resources = filteredResources.filter((resource) => {
+
+  for (const resource of resources) {
     const duplicate = seen.has(resource.id);
 
     seen.add(resource.id);
 
-    return !duplicate;
-  });
+    if (duplicate) {
+      continue;
+    }
 
-  const currentResource = null;
+    if (
+      resource.action &&
+      (resource.action === "delete" || resource.action === "oldVersion")
+    ) {
+      continue;
+    }
 
-  for (const resource of resources) {
+    allResources.push({ ...resource, versionName: resource.Version[0].name });
+    allResourceIds.push(resource.id);
+  }
+
+  // If a resource is cloned, remove the original resource from the list in favor of the cloned resource
+  for (let i = 0; i < allResources.length; i++) {
+    const resource = allResources[i];
+
+    if (resource.original_resource_id) {
+      const originalResourceIndex = allResources.findIndex(
+        (r) => r.id === resource.original_resource_id,
+      );
+
+      if (originalResourceIndex !== -1) {
+        allResources.splice(originalResourceIndex, 1);
+        i--;
+      }
+    }
+  }
+
+  for (const resource of allResources) {
     const item = {
       label: resource.title,
       latestCollectionVersionName: resource.versionName,
