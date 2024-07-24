33ead2dc76f71c2c3e2acf10268e281550d96140
Sanjay Soundarajan <sanjay.16@live.com>
2024-07-24 01:53:16 -0700

    ✨ feat:  update to a grid background

---
+++ components/grid/GridPattern.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { provide } from "vue";
+
+const props = defineProps({
+  offsetX: {
+    default: -1,
+    type: Number,
+  },
+  offsetY: {
+    default: -1,
+    type: Number,
+  },
+  size: {
+    default: 64,
+    type: Number,
+  },
+});
+
+const id = Math.round(Math.random() * 999);
+
+provide("grid-context", props);
+</script>
+
+<template>
+  <svg>
+    <defs>
+      <pattern
+        :id="`grid-pattern-${id}`"
+        viewBox="0 0 64 64"
+        :width="size"
+        :height="size"
+        patternUnits="userSpaceOnUse"
+        :x="offsetX"
+        :y="offsetY"
+      >
+        <path d="M64 0H0V64" fill="none" />
+      </pattern>
+    </defs>
+
+    <rect
+      width="100%"
+      height="100%"
+      stroke-width="0"
+      :fill="`url(#grid-pattern-${id})`"
+    ></rect>
+
+    <slot />
+  </svg>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -31,7 +31,88 @@ definePageMeta({
       </div>
     </div>
 
-    <section class="relative">
+    <section class="mx-auto flex items-center">
+      <div
+        class="relative flex w-full items-center justify-center overflow-hidden rounded-2xl"
+      >
+        <GridPattern
+          :offset-x="0"
+          :offset-y="0"
+          :size="64"
+          class="absolute -inset-px h-full w-full stroke-black/10 stroke-[4] [mask-image:radial-gradient(white,transparent_70%)] [stroke-dasharray:5_6] [stroke-dashoffset:10]"
+        >
+        </GridPattern>
+
+        <div class="font-display relative text-4xl font-bold">
+          <div
+            class="relative mx-auto max-w-screen-xl px-4 pt-8 text-center lg:px-12 lg:pt-16"
+          >
+            <h1
+              class="relative mb-4 bg-gradient-to-r from-amber-500 to-pink-500 bg-clip-text py-2 text-4xl font-extrabold text-transparent md:text-5xl lg:text-6xl"
+            >
+              Creating the connections that matter
+            </h1>
+
+            <p
+              class="mb-8 text-lg font-medium text-stone-700 sm:px-16 lg:text-xl xl:px-48 dark:text-stone-50"
+            >
+              Reimagining the links between scientific research outputs and the
+              people who need them.
+            </p>
+
+            <div
+              class="mb-8 flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-x-4 sm:space-y-0 lg:mb-16"
+            >
+              <NuxtLink
+                to="/dashboard"
+                class="inline-flex items-center justify-center rounded-lg bg-primary-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-primary-800 focus:ring-4 focus:ring-primary-300"
+              >
+                Create a collection
+                <svg
+                  class="-mr-1 ml-2 h-5 w-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </NuxtLink>
+
+              <a
+                href="#"
+                class="inline-flex items-center justify-center rounded-lg border border-gray-300 bg-white px-5 py-3 text-center text-base font-medium text-gray-900 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100"
+              >
+                <svg
+                  class="-ml-1 mr-2 h-5 w-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"
+                  ></path>
+                </svg>
+                Watch video
+              </a>
+            </div>
+
+            <div
+              class="mx-auto px-4 pb-5 text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36"
+            >
+              <span class="text-base font-medium uppercase text-gray-800">
+                Trusted by research teams around the world
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- <section class="relative hidden">
       <div
         class="relative mx-auto max-w-screen-xl px-4 pt-8 text-center lg:px-12 lg:pt-16"
       >
@@ -114,7 +195,7 @@ definePageMeta({
           </span>
         </div>
       </div>
-    </section>
+    </section> -->
 
     <div>
       <Vue3Marquee clone gradient>
